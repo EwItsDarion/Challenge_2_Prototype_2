d5671b24b82ced47ed6a1ee65f281d30d079fdf2
EwItsDarion <131904023+EwItsDarion@users.noreply.github.com>
2023-09-17 16:42:55 -0500

    Final Prototype 2 Product
    Without health addition

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabsToSpawn;
+
+    private float leftBound = -14;
+    private float rightBound = 14;
+    private float spawnPosZ = 20;
+
+    void Start()
+    {
+        //  InvokeRepeating("SpawnRandomPrefab", 2, 1.5f);
+
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+    }
+
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            SpawnRandomPrefab();
+
+            float randomDelay = Random.Range(0.8f, 3.0f);
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //SpawnRandomPrefab();
+        }
+    }
+
+
+    void SpawnRandomPrefab()
+    {
+        
+        {
+            //pick a random animal index
+            int prefabIndex = Random.Range(0, prefabsToSpawn.Length);
+
+            //generate random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, spawnPosZ);
+
+            //spawn the animal
+            Instantiate(prefabsToSpawn[prefabIndex], spawnPos, prefabsToSpawn[prefabIndex].transform.rotation);
+        }
+    }
+
+}
